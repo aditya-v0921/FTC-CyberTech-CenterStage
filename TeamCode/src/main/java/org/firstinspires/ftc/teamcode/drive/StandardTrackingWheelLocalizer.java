bc7a80c5d363017aa6ca2569fff16f88688f976b
aditya-v0921 <adityagv30@gmail.com>
2024-02-16 00:43:31 -0800

    Roadrunner Tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -35,7 +35,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double LATERAL_DISTANCE = 12.45; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = 7.2; // in; offset of the lateral wheel
 
-    public static double X_MULTIPLIER = 1.00281922; // Multiplier in the X direction
+    public static double X_MULTIPLIER = 1.002819221; // Multiplier in the X direction
     public static double Y_MULTIPLIER = 1.00991523; // Multiplier in the Y direction
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
