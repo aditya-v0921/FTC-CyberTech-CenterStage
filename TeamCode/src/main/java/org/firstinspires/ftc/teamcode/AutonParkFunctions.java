414b1f18c3e4c86c2baaea73e557abc05aa9c22b
Aadhith Rajinikanth <aadhithr18@gmail.com>
2024-02-18 20:44:19 -0800

    blah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonParkFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonParkFunctions.java
@@ -1,2 +1,16 @@
-package org.firstinspires.ftc.teamcode;public class AutonParkFunctions {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class AutonParkFunctions {
+    public DcMotorEx motorFrontLeft;
+    public DcMotorEx motorBackLeft;
+    private DcMotorEx motorFrontRight;
+    public DcMotorEx motorBackRight;
+    public DcMotorEx motorLeftLinear;
+    public DcMotorEx motorRightLinear;
+    public ElapsedTime runtime = new ElapsedTime();
+
+
 }
