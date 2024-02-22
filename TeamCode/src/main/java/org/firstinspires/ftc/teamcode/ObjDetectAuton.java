510edff318f210eb5270d69515b31f7f413ff206
aditya-v0921 <adityagv30@gmail.com>
2024-02-21 20:54:44 -0800

    Auton Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjDetectAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjDetectAuton.java
@@ -11,12 +11,11 @@ import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
 import java.util.List;
 
-@Autonomous(name = "Concept: TensorFlow Object Detection", group = "Concept")
+@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
 public class ObjDetectAuton extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;
-    //private static final String TFOD_MODEL_ASSET = "MyModelStoredAsAsset.tflite";
-    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/RedModel.tflite";
+    private static final String TFOD_MODEL_ASSET = "RedModel.tflite";
     private static final String[] LABELS = {
             "Center",
             "Left",
@@ -74,14 +73,14 @@ public class ObjDetectAuton extends LinearOpMode {
             telemetry.addData("Location Detected", "%s (%.0f %% Conf.)", label, confidence * 100);
 
             switch (label) {
-                case "Location1":
-                    executeCodeForLocation1();
+                case "Center":
+                    executeCodeForCenter();
                     break;
-                case "Location2":
-                    executeCodeForLocation2();
+                case "Left":
+                    executeCodeForLeft();
                     break;
-                case "Location3":
-                    executeCodeForLocation3();
+                case "Right":
+                    executeCodeForLeft();
                     break;
                 default:
                     telemetry.addData("Error", "Unknown location");
@@ -90,15 +89,15 @@ public class ObjDetectAuton extends LinearOpMode {
         }
     }
 
-    private void executeCodeForLocation1() {
-        telemetry.addData("Action", "Executing code for Location 1");
+    private void executeCodeForCenter() {
+        telemetry.addData("Action", "Executing code for Center");
     }
 
-    private void executeCodeForLocation2() {
-        telemetry.addData("Action", "Executing code for Location 2");
+    private void executeCodeForLeft() {
+        telemetry.addData("Action", "Executing code for Left");
     }
 
-    private void executeCodeForLocation3() {
-        telemetry.addData("Action", "Executing code for Location 3");
+    private void executeCodeForRight() {
+        telemetry.addData("Action", "Executing code for Right");
     }
 }
